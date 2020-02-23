c52bb3f644d711f6c5e25635281afe7ad7b0af89
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-02-23 13:25:03 +0300

    Работает

---
+++ HW2/2.2/IList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2_2
+{
+    interface IList
+    {
+
+        void Add(string value);
+
+        void RemoveByPosition(int position);
+
+        void RemoveByValue(string value);
+
+        void Print();
+
+        bool isEmpty();
+
+        int Length();
+    }
+}
---
+++ HW2/2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTable = new HashTable();
+
+            myTable.Add("Rabbit");
+            myTable.Add("Elephant");
+            myTable.Add("Wolf");
+            myTable.Add("Horse");
+            myTable.Add("Pig");
+            myTable.Add("Cow");
+            Console.WriteLine(!myTable.Contains("Cat"));
+            Console.WriteLine(myTable.Contains("Pig"));
+            myTable.Remove("Pig");
+            Console.WriteLine(!myTable.Contains("Pig"));
+        }
+    }
+}
