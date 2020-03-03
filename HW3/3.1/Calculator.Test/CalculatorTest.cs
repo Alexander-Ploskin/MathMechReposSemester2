6811794bd1a3e70e02c10ea20a97f3afe9a52b7c
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-03 16:57:52 +0300

    Create project for test

---
+++ HW3/3.1/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private Calculator calculator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            bool isSuccessful = true;
+        }
+
+        [TestMethod]
+        public void TestOfAdditionOfBigNumbers()
+        {
+            Assert.IsTrue(Cal)
+        }
+    }
+}
