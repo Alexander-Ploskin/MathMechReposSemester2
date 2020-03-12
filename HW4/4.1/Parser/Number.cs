6741f2af658ee0d27ed6668c55af1daa2c806125
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-12 19:39:53 +0300

    Created class for numbers in tree

---
+++ HW4/4.1/Parser/Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Number: INodeOfParserTree
+    {
+        private int value = 0;
+
+        public void Print()
+        {
+            Console.WriteLine($"{value} ");
+        }
+
+        public int Calculate()
+        {
+            return value;
+        }
+    }
+}
