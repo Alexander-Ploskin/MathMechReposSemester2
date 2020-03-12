88773b1dc007e34146f668c78a207a21e7df27f8
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-12 19:37:11 +0300

    Created class Operator

---
+++ HW4/4.1/Parser/Operator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    abstract class Operator: INodeOfParserTree
+    {
+        private Operator parent = null;
+        private INodeOfParserTree leftChild = null;
+        private INodeOfParserTree rightChild = null;
+
+        public Operator(Operator parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract void Print();
+
+        public abstract int Calculate();
+    }
+}
