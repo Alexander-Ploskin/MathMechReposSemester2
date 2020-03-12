8b917ec252fd15900baec679eb2edba5d14a0720
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-12 19:52:55 +0300

    Created classes for all operations

---
+++ HW4/4.1/Parser/Division.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Division: Operator
+    {
+        public Division(Operator parent)
+        {
+            this.parent = parent;
+        }
+
+        public override int Calculate()
+        {
+            if (Empty())
+            {
+                throw new Exception();
+            }
+
+            int valueOfRightChild = rightChild.Calculate();
+            if (valueOfRightChild == 0)
+            {
+                throw new Exception();
+            }
+
+            return leftChild.Calculate() / rightChild.Calculate();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("/ ");
+        }
+    }
+}
---
+++ HW4/4.1/Parser/Subtraction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    class Subtraction: Operator
+    {
+        public Subtraction(Operator parent)
+        {
+            this.parent = parent;
+        }
+
+        public override int Calculate()
+        {
+            if (Empty())
+            {
+                throw new Exception();
+            }
+
+            return leftChild.Calculate() - rightChild.Calculate();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("- ");
+        }
+    }
+}
