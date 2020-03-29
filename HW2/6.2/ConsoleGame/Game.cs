7c75c837dd155725bd9ec5b7ea308120b7ddb43f
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-29 21:09:50 +0300

    Made initialization of the game

---
+++ HW2/6.2/ConsoleGame/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleGame
+{
+    class Game
+    {
+        public Game(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                string line = "";
+                bool spawned = false;
+
+                while ((line = sr.ReadLine()) != null)
+                { 
+                    if (!spawned)
+                    {
+                        for (int i = 0; i < line.Length; ++i)
+                        {
+                            if (line[i] == ' ' && !spawned)
+                            {
+                                Console.Write('@');
+                                spawned = true;
+                                i++;
+                            }
+                            Console.Write(line[i]);
+                        }
+                        Console.WriteLine();
+                        continue;
+                    }
+
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
---
+++ HW2/6.2/ConsoleGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            Console.WriteLine("Enter the path of map file:");
+            path = Console.ReadLine();
+
+            try
+            {
+                var game = new Game(path);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
