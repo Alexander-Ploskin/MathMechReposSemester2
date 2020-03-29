6b25f6c75c5bcb2bc56f42a0a8e28dd3679b0445
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-29 18:17:56 +0300

    Did nothing

---
+++ HW2/6.1/ListFunctions/ListFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFunctions
+{
+    class MyList: List<int>
+    {
+        public List<int> Map(List<int> list, Func<int, int> func)
+        {
+            list.ForEach()
+            return list;
+        }
+
+        public List<int> Filter(List<int> list, Func<int, bool> func)
+        {
+            
+        }
+    }
+}
