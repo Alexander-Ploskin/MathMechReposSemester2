48aed7f325aaa371760a50e628c6e54f4f15f467
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-03-30 14:36:30 +0300

    Changed file name

---
+++ HW6/6.2/ConsoleGame/Game.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleGame
+{
+    class Game
+    {
+        public Game(string path)
+        {
+            int downDisplacement = 0;
+            int leftDisplacement = 0;
+
+            using (var sr = new StreamReader(path))
+            {
+                string line = "";
+                bool spawned = false;
+
+                while ((line = sr.ReadLine()) != null)
+                { 
+                    if (!spawned)
+                    {
+                        leftDisplacement = 0;
+
+                        for (int i = 0; i < line.Length; ++i)
+                        {
+                            if (line[i] == ' ' && !spawned)
+                            {
+                                Console.Write('@');
+                                spawned = true;
+                                i++;
+                            }
+
+                            if (!spawned)
+                            {
+                                leftDisplacement++;
+                            }
+
+                            Console.Write(line[i]);
+                        }
+
+                        Console.WriteLine();
+                        downDisplacement++;
+                        continue;
+                    }
+
+                    Console.WriteLine(line);
+                    downDisplacement++;
+                }
+            }
+
+            Console.CursorTop -= downDisplacement;
+            Console.CursorTop++;
+            Console.CursorLeft += leftDisplacement;
+        }
+
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft == 0)
+            {
+                return;
+            }
+
+            Console.CursorLeft--;
+            Console.Beep();
+        }
+
+        public void OnRight(object sender, EventArgs args)
+        {
+            Console.CursorLeft++;
+            Console.Beep();
+        }
+
+        public void OnDown(object sender, EventArgs args)
+        {
+            Console.CursorTop++;
+            Console.Beep();
+        }
+
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop == 0)
+            {
+                return;
+            }
+            Console.CursorTop--;
+            Console.Beep();
+        }
+
+    }
+}
