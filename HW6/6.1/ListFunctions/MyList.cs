9d06fd3265432ad25c5adec374b7138fab9653ca
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-04-01 23:35:03 +0300

    Create tests

---
+++ HW6/6.1/ListFunctions/MyList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFunctions
+{
+    public static class ListFunctions
+    {
+        public static List<int> Map(List<int> list,Func<int, int> func)
+        {
+            var output = new List<int>();
+
+            foreach (int item in list)
+            {
+                output.Add(func(item));
+            }
+
+            return output;
+        }
+
+        public static List<int> Filter(List<int> list, Func<int, bool> func)
+        {
+            foreach (int item in list)
+            {
+                if (func(item))
+                {
+                    list.Remove(item);
+                }
+            }
+
+            return list;
+        }
+
+        public static int Fold(List<int> list, int counter, Func<int, int, int> func)
+        {
+            foreach (int item in list)
+            {
+                counter = func(counter, item);
+            }
+
+            return counter;
+        }
+
+    }
+}
---
+++ HW6/6.1/ListFunctionsTests/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ListFunctionsTests
+{
+    [TestClass]
+    public class ListFunctionsTests
+    {
+        private List<int> list = null;
+
+        [TestInitialize]
+
+        public void Initialize()
+        {
+            list = new List<int>() { 0, 1, 2, 3, -5, 10, 0};
+        }
+
+        [TestMethod]
+        public void SimpleMapTest()
+        {
+            Assert.AreEqual(new List<int> { 0, 2, 4, 6, -10, 20, 0 }, ListFunctions.ListFunctions.Map(list, (x) => x * 2));
+            Assert.AreEqual(new List<int> { 0, 1, 4, 9, 25, 100, 0 }, ListFunctions.ListFunctions.Map(list, (x) => x * x));
+            Assert.AreEqual(new List<int> { -5, -4, -3, -2, -10, 5, -5 }, ListFunctions.ListFunctions.Map(list, (x) => x * 2));
+        }
+
+        [TestMethod]
+        public void MapEmptyListTest()
+        {
+            try
+            {
+                ListFunctions.ListFunctions.Map(new List<int> { }, (x) => 49 * x + x * x - 15);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void SimpleFilterTest()
+        {
+            Assert.AreEqual(new List<int> { 1, 2, 3, -5, 10 }, ListFunctions.ListFunctions.Filter(list, (x) => x != 0));
+            Assert.AreEqual(new List<int> { 2 }, ListFunctions.ListFunctions.Filter(list, (x) => x % 2 == 0));
+            Assert.AreEqual(new List<int> { }, ListFunctions.ListFunctions.Filter(list, (x) => x > 10));
+        }
+
+        [TestMethod]
+        public void FilterEmptyListTest()
+        {
+            try
+            {
+                ListFunctions.ListFunctions.Filter(new List<int> { }, (x) => x == 420);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void SimpleFoldTest()
+        {
+            Assert.AreEqual(6, ListFunctions.ListFunctions.Fold(new List<int> { 1, 2, 3 }, 1, (acc, elem) => acc * elem));
+            Assert.AreEqual(0, ListFunctions.ListFunctions.Fold(new List<int> { 2, -2 }, 0, (acc, elem) => acc + elem));
+            Assert.AreEqual(14, ListFunctions.ListFunctions.Fold(new List<int> { 1, 2, 3 }, 0, (acc, elem) => acc + elem * elem));
+        }
+
+        [TestMethod]
+        public void FoldEmptyListTest()
+        {
+            try
+            {
+                ListFunctions.ListFunctions.Fold(new List<int> { }, 0, (acc, elem) => acc + elem * elem);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
