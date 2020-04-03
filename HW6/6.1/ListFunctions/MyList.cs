d04519450b57a9513dc0c869eac612194f7f5b81
Alexander_Ploskin <Ploskin010&@gmail.com>
2020-04-03 16:21:31 +0300

    Wrote xml and fix the name of test proj

--- HW6/6.1/ListFunctions/MyList.cs
+++ HW6/6.1/ListFunctions/MyList.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace ListFunctions
 {
+    /// <summary>
+    /// Class implements three functions for List<int>
+    /// </summary>
     public static class ListFunctions
     {
+        /// <summary>
+        /// Aplicates func to all elements of list
+        /// </summary>
+        /// <param name="list">Input list</param>
+        /// <param name="func">Int function for elements</param>
+        /// <returns>List of transformed elements</returns>
         public static List<int> Map(List<int> list,Func<int, int> func)
         {
             var output = new List<int>();
@@ -20,6 +29,12 @@ namespace ListFunctions
             return output;
         }
 
+        /// <summary>
+        /// Create list, that has only elements, that fulfill the condition
+        /// </summary>
+        /// <param name="list">Input list</param>
+        /// <param name="func">Bool func that filtres list</param>
+        /// <returns>List of desired elements</returns>
         public static List<int> Filter(List<int> list, Func<int, bool> func)
         {
             foreach (int item in list)
@@ -33,6 +48,13 @@ namespace ListFunctions
             return list;
         }
 
+        /// <summary>
+        /// Aplicates func for all elements and calculate number
+        /// </summary>
+        /// <param name="list">Input list</param>
+        /// <param name="counter">Var that accumulates number</param>
+        /// <param name="func">Func that aplicates for all elements</param>
+        /// <returns>Number, that was calculated</returns>
         public static int Fold(List<int> list, int counter, Func<int, int, int> func)
         {
             foreach (int item in list)
