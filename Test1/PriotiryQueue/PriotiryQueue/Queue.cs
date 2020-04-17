e8d001a7cb3ea2befc934d8ee87246ebc8e7992f
Alexander-Ploskin <ploskin0107@gmail.com>
2020-04-17 16:52:28 +0300

    Made queue

---
+++ Test1/PriotiryQueue/PriotiryQueue/Queue.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriotiryQueue
+{
+    /// <summary>
+    /// Queue with priorities: First element - element with max key. Elements with same key in order of simple queue
+    /// </summary>
+    /// <typeparam name="T">Type of elements in queue</typeparam>
+    public class Queue<T>
+    {
+        /// <summary>
+        /// Linear implimentation of queue by list
+        /// </summary>
+        private class QueueList
+        {
+            /// <summary>
+            /// Elements in list
+            /// </summary>
+            private class ListElement
+            {
+                public ListElement(T value, int key, ListElement next)
+                {
+                    this.key = key;
+                    this.value = value;
+                    this.next = next;
+                }
+
+                public T value;
+                public int key;
+
+                public ListElement next;
+            }
+
+            private ListElement head;
+
+            /// <summary>
+            /// Adds element to tail of list
+            /// </summary>
+            /// <param name="value">Value of element</param>
+            /// <param name="key">Priority key of element</param>
+            public void Add(T value, int key)
+            {
+                if (head == null)
+                {
+                    head = new ListElement(value, key, null);
+                    return;
+                }
+
+                var currentElement = head;
+                while (currentElement.next != null)
+                {
+                    currentElement = currentElement.next;
+                }
+
+                currentElement.next = new ListElement(value, key, null);
+            }
+
+            /// <summary>
+            /// Remove element with max key
+            /// </summary>
+            /// <returns>Value of element with max key</returns>
+            public T ExtractMax()
+            {
+                if (head == null)
+                {
+                    throw new DequeueFromEmptyQueueException();
+                }
+
+                if (head.next == null)
+                {
+                    var output = head.value;
+                    head = null;
+                    return output;
+                }
+
+                int maxKey = head.key;
+                var parentOfMaxElement = head;
+                var currentElement = head;
+                while (currentElement.next != null)
+                {
+                    if (maxKey < currentElement.next.key)
+                    {
+                        maxKey = currentElement.next.key;
+                        parentOfMaxElement = currentElement;
+                    }
+                    currentElement = currentElement.next;
+                }
+
+                if (maxKey == head.key)
+                {
+                    var output = head.value;
+                    head = head.next;
+                    return output;
+                }
+
+                var result = parentOfMaxElement.next.value;
+                parentOfMaxElement.next = parentOfMaxElement.next.next;
+                return result;
+            }
+
+        }
+
+        private QueueList list;
+
+        public Queue()
+        {
+            list = new QueueList();
+        }
+
+        /// <summary>
+        /// Adds new element to queue
+        /// </summary>
+        /// <param name="value">Value of new element</param>
+        /// <param name="key">Priority key of new element</param>
+        public void Enqueue(T value, int key) => list.Add(value, key);
+
+        /// <summary>
+        /// Extract element with max key from queue
+        /// </summary>
+        /// <exception cref="DequeueFromEmptyQueueException">Throws when queue is empty</exception>
+        /// <returns>Value of element with max key</returns>
+        public T Dequeue() => list.ExtractMax();
+
+    }
+}
---
+++ Test1/PriotiryQueue/QueueTests/QueueTests.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+
+namespace QueueTests
+{
+    using PriotiryQueue;
+
+    public class Tests
+    {
+        private Queue<int> queue;
+
+        [SetUp]
+        public void Setup()
+        {
+            queue = new Queue<int>();
+        }
+
+        [Test]
+        public void DequeueFromEmptyTest()
+        {
+            try
+            {
+                queue.Dequeue();
+            }
+            catch (DequeueFromEmptyQueueException)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void SimpleDequeueTest()
+        {
+            queue.Enqueue(1, 1);
+            Assert.AreEqual(1, queue.Dequeue());
+        }
+
+        [Test]
+        public void AdditionOfThreeElementsWithSameKeyTest()
+        {
+            queue.Enqueue(1, 1);
+            queue.Enqueue(2, 1);
+            queue.Enqueue(3, 1);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(3, queue.Dequeue());
+        }
+
+        [Test]
+        public void AdditionOfTwoElementsWithNotSameKeyTest()
+        {
+            queue.Enqueue(2, 1);
+            queue.Enqueue(1, 3);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+        }
+
+        [Test]
+        public void AdditionOfTwoSameElementsTest()
+        {
+            queue.Enqueue(1, 1);
+            queue.Enqueue(1, 1);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(1, queue.Dequeue());
+        }
+
+        [Test]
+        public void MakeQueueEmptyTest()
+        {
+            queue.Enqueue(1, 1);
+            queue.Dequeue();
+            try
+            {
+                queue.Dequeue();
+            }
+            catch (DequeueFromEmptyQueueException)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void QueueTest()
+        {
+            queue.Enqueue(2, 1);
+            queue.Enqueue(3, 1);
+            queue.Enqueue(1, 2);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(3, queue.Dequeue());
+        }
+    }
+}
