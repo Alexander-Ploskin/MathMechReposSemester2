6500dd5b7480f1468975d62bd1f671261e1a4af9
Alexander-Ploskin <ploskin0107@gmail.com>
2020-04-18 17:01:55 +0300

    Add files

---
+++ HW3/3.1/CalculatorTests/StackTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace CalculatorTests
+{
+    using Calculator;
+
+    [TestFixture]
+    public class StackTests
+    {
+        private static IEnumerable<IStack> TestCases()
+        {
+            yield return new ListStack();
+            yield return new ArrayStack();
+        }
+
+        [TestCaseSource("TestCases")]
+        public void PushTest(IStack stack)
+        {
+            stack.Push(1);
+            Assert.IsFalse(stack.Empty());
+        }
+
+        [TestCaseSource("TestCases")]
+        public void PopTest(IStack stack)
+        {
+            stack.Push(1);
+            Assert.AreEqual(stack.Pop(), 1);
+        }
+
+        [TestCaseSource("TestCases")]
+        public void PopFromEmptyStackTest(IStack stack)
+        {
+            try
+            {
+                stack.Pop();
+            }
+            catch (PopFromEmptyStackException)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [TestCaseSource("TestCases")]
+        public void PushOfTwoElementsTest(IStack stack)
+        {
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(stack.Pop(), 2);
+            Assert.AreEqual(stack.Pop(), 1);
+        }
+
+    }
+}
