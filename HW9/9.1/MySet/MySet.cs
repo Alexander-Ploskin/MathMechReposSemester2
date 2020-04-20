9100479b63e5752335c53c085073622ccabab63c
Alexander-Ploskin <ploskin0107@gmail.com>
2020-04-20 11:12:49 +0300

    Start

---
+++ HW9/9.1/MySet/MySet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySet
+{
+    class MySet<T> : ISet<T>
+    {
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+
+        private class TreeElement
+        {
+            public TreeElement(TreeElement parent, T value)
+            {
+                Value = value;
+                Parent = parent;
+            }
+
+            public T Value { get; }
+            public TreeElement LeftChild { get; set; }
+            public TreeElement RightChild { get; set; }
+            public TreeElement Parent { get; set; }
+        } 
+
+
+    }
+}
---
+++ HW9/9.1/MySet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
