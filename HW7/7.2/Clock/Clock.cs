54cd74ca8d431f76c50e0ff84d8b8e3879b5b467
Alexander-Ploskin <ploskin0107@gmail.com>
2020-05-07 13:37:02 +0300

    Fixed working with coordinates

--- HW7/7.2/Clock/Clock.cs
+++ HW7/7.2/Clock/Clock.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Clock
 {
     /// <summary>
-    /// Analg circle clock, that displays system time of your PC
+    /// Analog circle clock, that displays system time of your PC
     /// </summary>
     public partial class Clock : Form
     {
@@ -17,15 +18,34 @@ namespace Clock
         private const int lengthOfMinuteHand = 110;
         private const int lengthOfHourHand = 80;
 
-        private int[] centerCoordinate = new int[2] { width / 2, height / 2}; 
+        private Coordinate centerCoordinate; 
 
-        Bitmap bitmap;
-        Graphics graphics;
+        private Bitmap bitmap;
+        private Graphics graphics;
 
+        private struct Coordinate
+        {
+            public int xCoordinate;
+            public int yCoordinate;
+
+            public Coordinate(int xCoordinate, int yCoordinate)
+            {
+                this.xCoordinate = xCoordinate;
+                this.yCoordinate = yCoordinate;
+            }
+        }
+
+
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
         public Clock()
         {
             InitializeComponent();
             bitmap = new Bitmap(width + 1, height + 1);
+            centerCoordinate = new Coordinate();
+            centerCoordinate.xCoordinate = width / 2;
+            centerCoordinate.yCoordinate = height / 2;
             BackGroundInitialize();
             timer.Interval = 1000;
             timer.Tick += new EventHandler(TimerTick);
@@ -52,10 +72,12 @@ namespace Clock
                     continue;
                 }
 
-                int[] coordinateOfStartPoint = CoordinateOfMinuteOrSecondHand(5 * i, 150);
-                int[] coordinateOfEndPoint = CoordinateOfMinuteOrSecondHand(5* i, 140);
-                graphics.DrawLine(new Pen(Color.Red, 1f), new Point(coordinateOfStartPoint[0], coordinateOfStartPoint[1]), new Point(coordinateOfEndPoint[0], coordinateOfEndPoint[1]));
+                var coordinateOfStartPoint = CoordinateOfMinuteOrSecondHand(5 * i, 150);
+                var coordinateOfEndPoint = CoordinateOfMinuteOrSecondHand(5 * i, 140);
+                graphics.DrawLine(new Pen(Color.Red, 1f), new Point(coordinateOfStartPoint.xCoordinate, coordinateOfStartPoint.yCoordinate), new Point(coordinateOfEndPoint.xCoordinate, coordinateOfEndPoint.yCoordinate));
             }
+
+            TimerTick(timer, EventArgs.Empty);
         }
 
         /// <summary>
@@ -71,16 +93,16 @@ namespace Clock
             int hour = DateTime.Now.Hour;
 
 
-            int[] handCoordinate = new int[2];
+            var handCoordinate = new Coordinate();
 
             handCoordinate = CoordinateOfMinuteOrSecondHand(second, lenghtOfSecondHand);
-            graphics.DrawLine(new Pen(Color.Red, 1f), new Point(centerCoordinate[0], centerCoordinate[1]), new Point(handCoordinate[0], handCoordinate[1]));
+            graphics.DrawLine(new Pen(Color.Red, 1f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
             handCoordinate = CoordinateOfMinuteOrSecondHand(minute, lengthOfMinuteHand);
-            graphics.DrawLine(new Pen(Color.Black, 2f), new Point(centerCoordinate[0], centerCoordinate[1]), new Point(handCoordinate[0], handCoordinate[1]));
+            graphics.DrawLine(new Pen(Color.Black, 2f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
             handCoordinate = CoordinateOfHourHand(hour % 12, minute, lengthOfHourHand);
-            graphics.DrawLine(new Pen(Color.Gray, 3f), new Point(centerCoordinate[0], centerCoordinate[1]), new Point(handCoordinate[0], handCoordinate[1]));
+            graphics.DrawLine(new Pen(Color.Gray, 3f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
             Front.Image = newBitmap;
         }
@@ -91,20 +113,20 @@ namespace Clock
         /// <param name="value">Number of minute if seconds</param>
         /// <param name="lengthOfHand">Length of hand</param>
         /// <returns>Coordinate of end point of hands</returns>
-        private int[] CoordinateOfMinuteOrSecondHand(int value, int lengthOfHand)
+        private Coordinate CoordinateOfMinuteOrSecondHand(int value, int lengthOfHand)
         {
-            int[] coordinate = new int[2];
+            var coordinate = new Coordinate();
             value *= 6;
 
             if (value >= 0 && value <= 180)
             {
-                coordinate[0] = centerCoordinate[0] + (int)(lengthOfHand * Math.Sin(Math.PI * value / 180));
-                coordinate[1] = centerCoordinate[1] - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
+                coordinate.xCoordinate = centerCoordinate.xCoordinate + (int)(lengthOfHand * Math.Sin(Math.PI * value / 180));
+                coordinate.yCoordinate = centerCoordinate.yCoordinate- (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
                 return coordinate;
             }
 
-            coordinate[0] = centerCoordinate[0] - (int)(lengthOfHand * -Math.Sin(Math.PI * value / 180));
-            coordinate[1] = centerCoordinate[1] - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
+            coordinate.xCoordinate = centerCoordinate.xCoordinate - (int)(lengthOfHand * -Math.Sin(Math.PI * value / 180));
+            coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
             return coordinate;
         }
 
@@ -115,21 +137,21 @@ namespace Clock
         /// <param name="minuteValue">Number of minutes</param>
         /// <param name="lengthOfHand">Length of hour hand</param>
         /// <returns>Coordinate of end point of hour hand</returns>
-        private int[] CoordinateOfHourHand(int hourValue, int minuteValue, int lengthOfHand)
+        private Coordinate CoordinateOfHourHand(int hourValue, int minuteValue, int lengthOfHand)
         {
-            int[] coordinate = new int[2];
+            var coordinate = new Coordinate();
 
             int value = (int)((hourValue * 30) + (minuteValue * 0.5));
 
             if (value >= 0 && value <= 180)
             {
-                coordinate[0] = centerCoordinate[0] + (int)(lengthOfHand * Math.Sin(Math.PI * value / 180));
-                coordinate[1] = centerCoordinate[1] - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
+                coordinate.xCoordinate = centerCoordinate.xCoordinate + (int)(lengthOfHand * Math.Sin(Math.PI * value / 180));
+                coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
             }
             else
             {
-                coordinate[0] = centerCoordinate[0] - (int)(lengthOfHand * -Math.Sin(Math.PI * value / 180));
-                coordinate[1] = centerCoordinate[1] - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
+                coordinate.xCoordinate = centerCoordinate.xCoordinate - (int)(lengthOfHand * -Math.Sin(Math.PI * value / 180));
+                coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(lengthOfHand * Math.Cos(Math.PI * value / 180));
             }
             return coordinate;
         }
